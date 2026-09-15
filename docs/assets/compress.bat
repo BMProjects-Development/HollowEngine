f97aa5b203d5e265a2007aa9d3eb73a12c670297
HollowHorizon <artemm@xaker.ru>
2026-09-15 12:33:48 +0300

    Add startup screenshots and update docs

--- docs/assets/compress.bat
+++ docs/assets/compress.bat
@@ -9,16 +9,4 @@ for /r %%i in (*.png) do (
     )
 )
 
-for /r %%i in (*.webp) do (
-    set "filename=%%~nxi"
-    if "!filename:~0,5!" NEQ "temp_" (
-        echo WEBP Optimization: "%%i"
-        ffmpeg -y -i "%%i" -c:v libwebp -quality 80 "%%~dptemp_%%~nxi" > nul 2>&1
-        if exist "%%~dptemp_%%~nxi" (
-            del "%%i"
-            move "%%~dptemp_%%~nxi" "%%i" > nul
-        )
-    )
-)
-
 pause
